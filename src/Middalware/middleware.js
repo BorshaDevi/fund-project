58ccd191fc1e50aae6177d8f6ebd2f47e484cd24
unknown <borshadevi27@gmail.com>
2025-04-25 23:23:13 +0600

    added middleware text

--- src/Middalware/middleware.js
+++ src/Middalware/middleware.js
@@ -1,14 +1,26 @@
+import verifyToken from "@/lib/auth"
+import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
 const routers=['/', '/employee']
-export default function middleware(req) {
+export default async function middleware(req) {
     try{
       const {pathname}=req.nextUrl
       const matchRoute=routers.some(route => pathname=== route || pathname.startsWith(route + '/'))
-      if(!matchRoute){
-        return NextResponse.redirect(new URL ('/login') , req.nextUrl)
-      }
       
+      if(matchRoute){
+        const token=(await cookies()).get('token').value
+        const user= token? await verifyToken(token) : null
+        if(!user){
+            if(pathname !== '/login'){
+                const loginUrl=new URL('/login',req.url)
+                loginUrl.searchParams().set('from',pathname)
+                return NextResponse.redirect(loginUrl)
+            }
+        }
+        return NextResponse.next()
+
+      }
     }catch(err){
         console.error('Middleware error' , err)
     }
