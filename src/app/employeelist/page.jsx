7fd768dadbe7aa21d6e942d9c53c3058ee164ba7
unknown <borshadevi27@gmail.com>
2025-05-25 22:28:22 +0600

    click Add Employee go to employee form

--- src/app/employeelist/page.jsx
+++ src/app/employeelist/page.jsx
@@ -7,7 +7,7 @@ const page=()=>{
         <div className="mx-auto max-w-3xl py-10 min-h-screen  sm:px-6 lg:px-10">
             <div className="bg-white shadow-xl rounded-lg p-6 text-center">
                 <div className="flex justify-end">
-                    <Link href={'/employeelist'}><button className="bg-green-800 p-2 text-white rounded-md font-semibold hover:bg-green-400" >Add Employee</button></Link>
+                    <Link href={'/employee'}><button className="bg-green-800 p-2 text-white rounded-md font-semibold hover:bg-green-400" >Add Employee</button></Link>
                 </div>
                 <div>Employee List</div>
             <EmployeeList></EmployeeList>
