6b1d4fc99629eefdc50cc43ac8fd962c6066f872
unknown <borshadevi27@gmail.com>
2025-05-25 23:35:39 +0600

    added data

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 const { Sidebar } = require("./ui/sidebar")
 
 const AppSidebar=()=>{
     return(
         <Sidebar>
-
+            <div>
+                <h1 className="text-green-900 font-extrabold text-xl font-serif">Provident Fund</h1>
+            </div>
+            <div className="mx-auto flex flex-col text-black justify-center items-center mt-10 space-y-5">
+            <Link href=''  >Employee List</Link>
+            <Link href=''  >User Profile</Link>
+            </div>
         </Sidebar>
     )
 }
