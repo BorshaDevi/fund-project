71ce5de658de1aee1b881a0eaa2dc241d5c61dfb
unknown <borshadevi27@gmail.com>
2025-05-25 14:46:23 +0600

    add button in employeelist page

--- src/app/employeelist/page.jsx
+++ src/app/employeelist/page.jsx
@@ -1,10 +1,14 @@
 import EmployeeList from "@/compent/EmployeeList";
+import Link from "next/link";
 
 
 const page=()=>{
     return(
         <div className="mx-auto max-w-3xl py-10 min-h-screen  sm:px-6 lg:px-10">
             <div className="bg-white shadow-xl rounded-lg p-6 text-center">
+                <div className="flex justify-end">
+                    <Link href={'/employeelist'}><button className="bg-green-800 p-2 text-white rounded-md font-semibold hover:bg-green-400" >Add Employee</button></Link>
+                </div>
                 <div>Employee List</div>
             <EmployeeList></EmployeeList>
         </div>
