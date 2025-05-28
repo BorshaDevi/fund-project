371a3f4f63ac9fb0a8ccbc708f9b406fd329dab2
unknown <borshadevi27@gmail.com>
2025-05-28 14:56:07 +0600

    added text

--- src/compent/UseProfile.jsx
+++ src/compent/UseProfile.jsx
@@ -1,17 +1,21 @@
-'use client';
-const UseProfile=({user})=>{
-    console.log(user, 'user profile');
-    return(
-        <div>
-             <h1 className="text-2xl font-bold text-center mt-10">User Profile</h1>
-            <div className="max-w-md mx-auto mt-5 p-5 bg-white rounded-lg shadow-md">
-                <img src='' alt='user Profile' className="text-center rounded border-green-600"></img>
-                <h2 className="text-xl font-semibold mb-4">Profile Details</h2>
-                <p className="text-gray-700 uppercase">Name: {user.name}</p>
-               
-        </div>
-        </div>
-    )
-}
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const UseProfile = ({ user }) => {
+  console.log(user, "user profile");
+  return (
+    <div >
+      <h1 className="text-2xl font-bold text-center mt-10">User Profile</h1>
+      <div className="max-w-md mx-auto mt-5 p-5 bg-white rounded-lg shadow-md text-center">
+        <Avatar className='mx-auto'>
+          <AvatarImage src="" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <h2 className="text-xl font-semibold mb-4">Profile Details</h2>
+        <p className="text-gray-700 uppercase">Name: {user.name}</p>
+      </div>
+    </div>
+  );
+};
 export default UseProfile;
 // Compare this snippet from src/app/userprofile/page.jsx:
