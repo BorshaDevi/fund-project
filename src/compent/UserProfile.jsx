c42f9acbf3e015bf190f64a21723307c2ff611ff
unknown <borshadevi27@gmail.com>
2025-06-05 12:53:32 +0600

    add data

--- src/compent/UserProfile.jsx
+++ src/compent/UserProfile.jsx
@@ -6,19 +6,24 @@ import { useEffect, useState } from "react";
 
 const UserProfile = ({ user }) => {
   const [data, setData] = useState({});
-  const [ image, setImage]=useState(null)
-  const [resource , setResource]=useState()
-  console.log(resource)
+  const [img, setImg] = useState(null);
+  const [resource, setResource] = useState();
+  console.log(resource);
   console.log(data, "data ");
-  console.log(name , 'name state data')
+  console.log(img, "image state data");
   const { userId } = user;
+
   // handleName button
-  const handleName=(e)=>{
-    e.preventDefault()
-    const name=e.target.name.value
-    axios.patch('/api/name')
-       console.log (name ,'hadle Name')
-  }
+  const handleName = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    let payload = {};
+    if (img) {
+      payload.image=img
+    }
+    if(name){payload.name=name}
+    axios.patch("/api/updateUser",payload);
+  };
 
   // userProfile get user data id aways
   useEffect(() => {
@@ -42,30 +47,29 @@ const UserProfile = ({ user }) => {
     fetchData();
   }, [userId]);
 
-
-
   return (
     <div>
       <h1 className="text-2xl font-bold text-center mt-10">User Profile</h1>
       <div className="max-w-md mx-auto mt-5 p-5 bg-white rounded-lg shadow-md text-center">
         <Avatar className="mx-auto bg-whtie border-green-700 border-spacing-4">
-          <AvatarImage src="" />
-          <AvatarFallback >Your Image</AvatarFallback>
+          <AvatarImage src={data?.image} />
+          <AvatarFallback>Your Image</AvatarFallback>
         </Avatar>
 
         {/* Image upload Update */}
-        <CldUploadWidget signatureEndpoint="/api/uploadImage"
-         onSuccess={(result , {widget})=>{
-          setResource(result?.info)
-         }}
-         onQueuesEnd={(result , {widget}) => {
-          widget.close();
-         }}
+        <CldUploadWidget
+          signatureEndpoint="/api/uploadImage"
+          onSuccess={(result, { widget }) => {
+            setResource(result?.info);
+          }}
+          onQueuesEnd={(result, { widget }) => {
+            widget.close();
+          }}
         >
           {({ open }) => {
-            function handleOnClick(){
-              setResource(undefined)
-              open()
+            function handleOnClick() {
+              setResource(undefined);
+              open();
             }
             return (
               <button
@@ -81,14 +85,15 @@ const UserProfile = ({ user }) => {
         {/* Name Update */}
         <form onSubmit={handleName} className="mt-5 mb-5">
           <input
-          
             type="text"
-            name='name'
+            name="name"
             className="border-b border-green-300 w-full outline-none focus:outline-none py-4 bg-transparent my-4 text-black uppercase font-semibold "
             defaultValue={data?.name}
           />
           <br></br>
-          <button className="p-1 font-bold text-white bg-green-500 rounded-md shadow-sm w-14"><input type="submit"  value='Save'/></button>
+          <button className="p-1 font-bold text-white bg-green-500 rounded-md shadow-sm w-14">
+            <input type="submit" value="Save" />
+          </button>
         </form>
       </div>
     </div>
