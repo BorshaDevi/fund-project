348cfb34d77fa21cee99ab31ff7a84c09076ce6d
unknown <borshadevi27@gmail.com>
2025-06-13 13:01:37 +0600

    added text

--- src/app/providentfund/[id]/page.jsx
+++ src/app/providentfund/[id]/page.jsx
@@ -2,11 +2,8 @@ import ProvidentFund from "@/compent/ProvidentFund";
 
 
 const ProvidentFundPage=()=>{
-   
     return (
         <div>
-            <h1 className="text-2xl font-bold text-center my-4">Provident Fund Management</h1>
-            <p className="text-center mb-8">Manage employee provident fund contributions and withdrawals.</p>
             <ProvidentFund></ProvidentFund>
         </div>
     )
--- src/compent/ProvidentFund.jsx
+++ src/compent/ProvidentFund.jsx
@@ -5,7 +5,7 @@ const ProvidentFund= () => {
     const {id}=useParams();
   console.log("Provident Fund ID:", id);
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
       <h1 className="text-4xl font-bold mb-4">Provident Fund</h1>
       <p className="text-lg text-gray-700">This page is under construction.</p>
     </div>
