daee316f30cb72586f1291ddce6d4274d766a9d4
unknown <borshadevi27@gmail.com>
2025-06-15 13:01:03 +0600

    added text

--- src/compent/ProvidentFund.jsx
+++ src/compent/ProvidentFund.jsx
@@ -1,7 +1,9 @@
 'use client';
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Link from "next/link";
 import { useParams } from "next/navigation";
+import { IoMdDownload } from "react-icons/io";
 
 const  ProvidentFund = () => {
     const {id}=useParams();
@@ -19,12 +21,22 @@ const  ProvidentFund = () => {
   });
   console.log(data, "Provident Fund  User data");
   return (
-    <div className="mx-auto max-w-3xl py-10 min-h-screen  sm:px-6 lg:px-10" >
-      <div className="flex flex-col items-center  h-screen bg-green-700">
-        <h1 className="text-2xl font-bold mb-4">Provident Fund</h1>
+    <div className="mx-auto max-w-6xl py-10 min-h-screen  sm:px-6 lg:px-5" >
+      <div className="bg-gradient-to-r from-green-500 to-emerald-300 shadow-xl rounded-lg text-center">
+        <div className='flex justify-end gap-10 p-4'>
+          <div className="">
+                    <Link href={`/providentfundform/${id}`}><button className="bg-green-800 p-2 text-white rounded-md font-semibold hover:bg-white hover:text-black " > Add Provident Fund</button></Link>
+                </div>
+        <div className="">
+                    <Link href={`/providentfundform/${id}`}><button className="bg-green-800 p-2 text-white rounded-md font-semibold hover:bg-green-400 flex" > <IoMdDownload className="mt-1 mr-1" />Download</button></Link>
+                </div>
+        </div>
+      <div className="flex flex-col items-center   h-screen bg-white ">
+        <h1 className="text-2xl font-bold mb-4 text-emerald-800">Provident Fund</h1>
       <p className="text-lg">Name:{data?.firstName} {data?.lastName}</p>
       <p className="text-lg">Designation:{data?.designation} </p>
       <p className="text-lg">EID:{data?.employeeId} </p>
+      </div>
       </div>
 
     </div>
